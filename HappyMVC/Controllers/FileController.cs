9edfa0e9aea6a86c7abeb8574bb0e7569526efbe
naman-sharma <namansharma@live.ca>
2017-09-18 14:37:22 -0700

    new filecontroller

---
+++ HappyMVC/Controllers/FileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyMVC.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            return View(files);
+        }
+
+        public ActionResult Content(string file)
+        {
+            string txt = System.IO.File.ReadAllText(file);
+            return View("Content", (object)txt);
+        }
+    }
+}
--- HappyMVC/Controllers/HomeController.cs
+++ HappyMVC/Controllers/HomeController.cs
@@ -20,11 +20,21 @@ namespace HappyMVC.Controllers
             return View();
         }
 
+        public ActionResult Foo()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+    
+
     }
 }
